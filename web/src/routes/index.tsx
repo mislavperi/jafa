548a44d7d160dd340f69c86b9f4e17a3b99c1f9f
mislavperi <peric.misalv@proton.me>
2026-01-08 01:40:31 +0100

    feat: add area graph

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,14 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {
-  Zap,
-  Server,
-  Route as RouteIcon,
-  Shield,
-  Waves,
-  Sparkles,
-} from 'lucide-react'
 
-import { Pie, PieChart, PieLabelRenderProps, Cell, Legend } from 'recharts'
+import { Pie, PieChart, AreaChart, Area, YAxis, XAxis, CartesianGrid, Tooltip, PieLabelRenderProps, Cell, Legend } from 'recharts'
 
 export const Route = createFileRoute('/')({ component: App })
 
@@ -66,6 +58,41 @@ function App() {
         </Pie>
         <Legend layout='vertical' />
       </PieChart>
+      <AreaChart
+        style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+        responsive
+        data={data}
+        margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="expenseType" />
+        <YAxis width="auto" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="uv"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+        <Area
+          type="monotone"
+          dataKey="pv"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+      </AreaChart>
     </div>
   )
 }
